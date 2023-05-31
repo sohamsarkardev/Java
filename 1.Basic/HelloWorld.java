689a1500affc1bc7efff746c995f418faa9d6310
sohamsarkardev <sohamsarkardev@gmail.com>
2023-06-01 01:48:28 +0530

    Hello World

---
+++ 1.Basic/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * 1.HelloWorld
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("value1:"+args[0]);
+        System.out.println("value2"+args[1]);
+        System.out.println("value3"+args[2]);
+      //  System.out.println("value4"+args[]);
+    }
+}
